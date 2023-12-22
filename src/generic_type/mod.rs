36c98c9fdeae200de7b83863b798af9adf4c3350
Dragonchu <dr.dragonchu@gmail.com>
2023-12-22 12:41:18 +0800

    优化函数

--- src/generic_type/mod.rs
+++ src/generic_type/mod.rs
@@ -2,19 +2,25 @@ pub mod basic {
     pub fn example() {
         println!("Finding the largest number in a list of numbers");
         let number_list = vec![34, 50, 25, 100, 65];
-        find_largest_number(&number_list);
-        println!("{:?}", number_list);
+
+        let result = largest(&number_list);
+        println!("The largest number is {}", result);
+
+        let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+        let result = largest(&number_list);
+        println!("The largest number is {}", result);
     }
 
-    fn find_largest_number(number_list: &Vec<i32>) {
-        let mut largest = &number_list[0];
+    fn largest(list: &[i32]) -> &i32 {//入参是一个切片slice
+        let mut largest = &list[0];
 
-        for number in number_list {
-            if number > largest {
-                largest = number;
+        for item in list {
+            if item > largest {
+                largest = item;
             }
         }
 
-        println!("The largest number is {}", largest);
+        largest
     }
 }
