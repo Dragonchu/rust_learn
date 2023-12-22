08d1fe20f454928dd2ad44ed2d82efeb2eecf31e
Dragonchu <dr.dragonchu@gmail.com>
2023-12-22 12:26:10 +0800

    增加test模块和测试方法

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,37 @@ use crate::error_handling::use_result::result_usage;
 
 use std::env;
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let degree = &args[1];
